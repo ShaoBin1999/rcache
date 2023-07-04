cd41c9b87a214ac38891ea82798d477521f040dd
renshaobin <renshaobin.rsb@alibaba-inc.com>
2023-07-04 14:19:39 +0800

    expand

--- cache1/src/main/java/com/bsren/cache/LocalCache.java
+++ cache1/src/main/java/com/bsren/cache/LocalCache.java
@@ -1,4 +1,5 @@
 package com.bsren.cache;
+
 import com.google.common.base.Ticker;
 import com.google.errorprone.annotations.concurrent.GuardedBy;
 
@@ -9,8 +10,9 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * 1.构造参数：cache最大数量
  * 2.实现Cache中的方法
+ * 3.expand
  */
-public class LocalCache<K,V>{
+public class LocalCache<K, V> {
 
     static final int MAXIMUM_CAPACITY = 1 << 30;
 
@@ -24,34 +26,34 @@ public class LocalCache<K,V>{
 
     Segment<K, V>[] segments;
 
-    public LocalCache(int maxWeight){
-        this.maxWeight = Math.min(MAXIMUM_CAPACITY,maxWeight);
+    public LocalCache(int maxWeight) {
+        this.maxWeight = Math.min(MAXIMUM_CAPACITY, maxWeight);
         int segmentShift = 0;
         int segmentCount = 1;
-        while (segmentCount*20<maxWeight){
+        while (segmentCount * 20 < maxWeight) {
             segmentShift++;
-            segmentCount<<=1;
+            segmentCount <<= 1;
         }
         this.segments = new Segment[segmentCount];
         this.segmentShift = 32 - segmentShift;
-        this.segmentMask = segmentCount-1;
-        int segmentCapacity = (this.maxWeight+segmentCount-1)/segmentCount;
+        this.segmentMask = segmentCount - 1;
+        int segmentCapacity = (this.maxWeight + segmentCount - 1) / segmentCount;
         int segmentSize = 1;
-        while (segmentSize<segmentCapacity){
-            segmentSize<<=1;
+        while (segmentSize < segmentCapacity) {
+            segmentSize <<= 1;
         }
-        for (int i=0;i<segments.length;i++){
+        for (int i = 0; i < segments.length; i++) {
             segments[i] = createSegment(segmentSize);
         }
     }
 
-    private Segment<K,V> createSegment(int segmentSize) {
+    private Segment<K, V> createSegment(int segmentSize) {
         return new Segment<>(this, segmentSize);
     }
 
     static class Segment<K, V> extends ReentrantLock {
 
-        LocalCache<K,V> map;
+        LocalCache<K, V> map;
 
         int segmentWeight;
 
@@ -66,45 +68,45 @@ public class LocalCache<K,V>{
 
         AtomicReferenceArray<Entry<K, V>> table;
 
-        Segment(LocalCache<K,V> map,int initialCapacity){
+        Segment(LocalCache<K, V> map, int initialCapacity) {
             this.map = map;
             this.segmentWeight = initialCapacity;
             initTable(initialCapacity);
         }
 
         private void initTable(int initialCapacity) {
-            AtomicReferenceArray<Entry<K,V>> newTable = new AtomicReferenceArray<>(initialCapacity);
-            this.threshold = newTable.length() * 3/4;
+            AtomicReferenceArray<Entry<K, V>> newTable = new AtomicReferenceArray<>(initialCapacity);
+            this.threshold = newTable.length() * 3 / 4;
             this.table = newTable;
         }
 
         public V get(Object key, int hash) {
             try {
-                if(count==0){
+                if (count == 0) {
                     return null;
                 }
-                AtomicReferenceArray<Entry<K,V>> table = this.table;
+                AtomicReferenceArray<Entry<K, V>> table = this.table;
                 Entry<K, V> first = table.get(hash & (table.length() - 1));
-                for (Entry<K,V> e = first; e!=null; e = e.getNext()){
-                    if(e.getHash() != hash){
+                for (Entry<K, V> e = first; e != null; e = e.getNext()) {
+                    if (e.getHash() != hash) {
                         continue;
                     }
                     K entryKey = e.getKey();
-                    if(equalsKey(entryKey,key)){
+                    if (equalsKey(entryKey, key)) {
                         Object valueReference = e.getValueReference();
-                        if(!(valueReference instanceof Value)){
+                        if (!(valueReference instanceof Value)) {
                             recordRead(e);
                             return (V) valueReference;
                         }
-                        V value = ((Value<K,V>) valueReference).get();
-                        if(value!=null){
+                        V value = ((Value<K, V>) valueReference).get();
+                        if (value != null) {
                             recordRead(e);
                             return value;
                         }
                     }
                 }
                 return null;
-            }finally {
+            } finally {
                 postRead();
             }
         }
@@ -118,55 +120,99 @@ public class LocalCache<K,V>{
         }
 
         private boolean equalsKey(K entryKey, Object key) {
-            return entryKey == key;
+            return entryKey.equals(key);
         }
 
         public V put(K key, int hash, V value) {
             lock();
             try {
                 int newCount = this.count;
-                if(newCount+1>threshold){
+                if (newCount + 1 > threshold) {
                     expand();
                 }
-                AtomicReferenceArray<Entry<K,V>> table = this.table;
-                int index = hash & (table.length()-1);
-                Entry<K,V> first = table.get(index);
-                for (Entry<K,V> e = first;e!=null;e = e.getNext()){
+                AtomicReferenceArray<Entry<K, V>> table = this.table;
+                int index = hash & (table.length() - 1);
+                Entry<K, V> first = table.get(index);
+                for (Entry<K, V> e = first; e != null; e = e.getNext()) {
                     K entryKey = e.getKey();
-                    if(equalsKey(entryKey,key)){
+                    if (equalsKey(entryKey, key)) {
                         Object valueReference = e.getValueReference();
                         V entryValue = null;
-                        if(!(valueReference instanceof Value)){
+                        if (!(valueReference instanceof Value)) {
                             entryValue = (V) valueReference;
                         } else {
-                            entryValue = (V) ((Value)valueReference).get();
+                            entryValue = (V) ((Value) valueReference).get();
                         }
                         modCount++;
-                        setValue(e,key,value);
+                        setValue(e, key, value);
                         this.count = newCount;
                         return entryValue;
                     }
                 }
                 modCount++;
-                Entry<K,V> entry = new StrongEntry<>(key,hash,first);
-                setValue(entry,key,value);
-                table.set(index,entry);
-                newCount = this.count+1;
+                Entry<K, V> entry = new StrongEntry<>(key, hash, first);
+                setValue(entry, key, value);
+                table.set(index, entry);
+                newCount = this.count + 1;
                 this.count = newCount;
                 return null;
-            }finally {
+            } finally {
                 unlock();
                 postWrite();
             }
         }
 
         @GuardedBy("this")
-        private void setValue(Entry<K,V> e, K key, V value) {
+        private void setValue(Entry<K, V> e, K key, V value) {
             e.setValue(value);
         }
 
+        @GuardedBy("this")
         private void expand() {
+            AtomicReferenceArray<Entry<K, V>> oldTable = this.table;
+            int oldCapacity = oldTable.length();
+            if (oldCapacity >= MAXIMUM_CAPACITY) {
+                return;
+            }
+            int newCount = this.count;
+            AtomicReferenceArray<Entry<K, V>> newTable = new AtomicReferenceArray<>(oldCapacity << 1);
+            this.threshold = newTable.length() * 3 / 4;
+            int newMask = newTable.length() - 1;
+            for (int oldIndex = 0; oldIndex < oldCapacity; oldIndex++) {
+                Entry<K, V> head = oldTable.get(oldIndex);
+                if(head==null){
+                    continue;
+                }
+                Entry<K, V> next = head.getNext();
+                int headIndex = head.getHash() & newMask;
+                if(next==null){
+                    newTable.set(headIndex,head);
+                }else {
+                    Entry<K,V> tail = head;
+                    int tailIndex = headIndex;
+                    for (Entry<K,V> e = next;e != null; e = e.getNext()){
+                        int newIndex = e.getHash() & newMask;
+                        if(newIndex != tailIndex){
+                            tailIndex = newIndex;
+                            tail = e;
+                        }
+                    }
+                    newTable.set(tailIndex,tail);
 
+                    for (Entry<K,V> e = head;e!=tail;e = e.getNext()){
+                        int newIndex = e.getHash() & newMask;
+                        Entry<K,V> newNext = newTable.get(newIndex);
+                        Entry<K,V> newFirst = copyEntry(e,newNext);
+                        newTable.set(newIndex,newFirst);
+                    }
+                }
+            }
+            table = newTable;
+            this.count = newCount;
+        }
+
+        private Entry<K,V> copyEntry(Entry<K,V> original, Entry<K,V> newNext) {
+            return new StrongEntry<>(original.getKey(),original.getHash(),newNext);
         }
 
         private void postWrite() {
@@ -174,14 +220,14 @@ public class LocalCache<K,V>{
         }
     }
 
-    public V getIfPresent(Object key){
+    public V getIfPresent(Object key) {
         int hash = hash(key);
-        return segmentFor(hash).get(key,hash);
+        return segmentFor(hash).get(key, hash);
     }
 
-    public V put(K key,V value){
+    public V put(K key, V value) {
         int hash = hash(key);
-        return segmentFor(hash).put(key,hash,value);
+        return segmentFor(hash).put(key, hash, value);
     }
 
 
--- cache1/src/main/java/com/bsren/cache/TestCache.java
+++ cache1/src/main/java/com/bsren/cache/TestCache.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 public class TestCache {
 
     @Test
-    public void test1(){
+    public void testGetAndSet(){
         LocalCache<String,String> localCache = new LocalCache<>(100);
         localCache.put("rsb","24");
         System.out.println(localCache.getIfPresent("rsb"));
+    }
+
+    @Test
+    public void testExpand(){
+        LocalCache<String,String> localCache = new LocalCache<>(10);
+        for (int i=0;i<100;i++){
+            localCache.put("case"+i,i+"0");
+        }
+        System.out.println(localCache.getIfPresent("case"+99));
     }
 }
